3fd64fbfd6761bf6e8769d47ba06f299b368b463
DiegoRafaelAF <draf0314@gmail.com>
2020-05-12 17:45:50 -0300

    Code Climate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
-import * as api from './services/api'
 
 function App() {
   return (
     <div className="App">
-      <p>projeto</p>
+      {/* <p>projeto</p> */}
     </div>
   );
 }
