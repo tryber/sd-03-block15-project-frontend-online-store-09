5e7ef7b3db1f4d60e4e54e41d8d893537283005f
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-05-13 14:48:44 -0300

    Codeclimate check

--- src/components/ShoppingCartButton.js
+++ src/components/ShoppingCartButton.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import icon from './icon.svg';
+import React from "react";
+import { Link } from "react-router-dom";
+import icon from "./icon.svg";
 
 class ShoppingCartButton extends React.Component {
-
   render() {
     return (
-      <div data-testid="shopping-cart-button" >
+      <div data-testid="shopping-cart-button">
         <Link to="/cart">
-          <img src={icon} style={{marginLeft: "20px"}} alt="shopping cart icon" width="50px" />
+          <img
+            src={icon}
+            style={{ marginLeft: "20px" }}
+            alt="shopping cart icon"
+            width="50px"
+          />
         </Link>
-          <p style={{width: "100px", textAlign: "center"}} data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+        <p
+          style={{ width: "100px", textAlign: "center" }}
+          data-testid="shopping-cart-empty-message"
+        >
+          Seu carrinho está vazio
+        </p>
       </div>
     );
   }
