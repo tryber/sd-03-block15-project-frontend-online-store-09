f00973d09cc15516b8824b929a029f5267564738
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-05-13 14:50:49 -0300

    Codeclimate check

--- src/components/ShoppingCartButton.js
+++ src/components/ShoppingCartButton.js
@@ -9,13 +9,13 @@ class ShoppingCartButton extends React.Component {
         <Link to="/cart">
           <img
             src={icon}
-            style={{ marginLeft: "20px" }}
+            style={ { marginLeft: '20px' } }
             alt="shopping cart icon"
             width="50px"
           />
         </Link>
         <p
-          style={{ width: "100px", textAlign: "center" }}
+          style={ { width: '100px', textAlign: 'center' } }
           data-testid="shopping-cart-empty-message"
         >
           Seu carrinho está vazio
