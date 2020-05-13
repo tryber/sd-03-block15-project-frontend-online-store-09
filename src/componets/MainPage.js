e48c59a1672b11af6f3c1c263f3ed713ed84e0ee
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-05-13 09:58:56 -0300

    Codeclimate check

--- src/componets/MainPage.js
+++ src/componets/MainPage.js
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
-import ShoppingCartButton from "./ShoppingCartButton";
+import React, { Component } from 'react';
+import ShoppingCartButton from './ShoppingCartButton';
 
 export class MainPage extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { search: "" };
+    this.state = { search: '' };
     this.handleChange = this.handleChange.bind(this);
   }
 
@@ -22,7 +22,7 @@ export class MainPage extends Component {
           justifyContent: "space-between",
           padding: "10px 10px 5px 10px",
           border: "1px solid black",
-          margin: "5px 5px 5px 5px"
+          margin: "5px 5px 5px 5px",
         }}
       >
         <header>
