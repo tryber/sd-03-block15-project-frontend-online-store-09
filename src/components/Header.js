73f30aab7a144fb9fbd13305b325c447014351b2
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-05-13 15:13:07 -0300

    Codeclimate check

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,34 @@
-import React, { Component } from 'react'
-import Category from './Category'
+import React, { Component } from 'react';
+import Category from './Category';
 
- class Header extends Component {
-   constructor(props) {
-  super(props);
+class Header extends Component {
+  constructor(props) {
+    super(props);
 
-  this.state = { search: '' };
-  this.handleChange = this.handleChange.bind(this);
-}
+    this.state = { search: "" };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-handleChange(event) {
-  const { value } = event.target;
-  this.setState({ search: value });
-}
+  handleChange(event) {
+    const { value } = event.target;
+    this.setState({ search: value });
+  }
 
   render() {
     return (
       <header>
-      <div>
-        <p data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
-        <input value={this.state.search} onChange={this.handleChange} />
-      </div>
-      <div>
-        <Category />
-      </div>
-    </header>
-    )
+        <div>
+          <p data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </p>
+          <input value={this.state.search} onChange={this.handleChange} />
+        </div>
+        <div>
+          <Category />
+        </div>
+      </header>
+    );
   }
 }
-
 
 export default Header;
