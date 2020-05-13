37f45cb951c85564255d31373ba86f56cfcbdaff
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-05-12 22:37:52 -0300

    Codeclimate check

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path='/' component={MainPage} />
+        <Route exact path="/" component={MainPage} />
         {/* <Route path='/cart' component={CartButton} /> */}
       </Switch>
     </Router>
