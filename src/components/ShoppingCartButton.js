d505148c4fcc0871e95c0a2fd118d05d4c44f379
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-05-13 14:45:54 -0300

    3rd Req. Resolved the issue with data-testid not seeing the message properly.

--- src/components/ShoppingCartButton.js
+++ src/components/ShoppingCartButton.js
@@ -8,8 +8,9 @@ class ShoppingCartButton extends React.Component {
     return (
       <div data-testid="shopping-cart-button" >
         <Link to="/cart">
-          <img src={icon} alt="shopping cart icon" width="50px" />
+          <img src={icon} style={{marginLeft: "20px"}} alt="shopping cart icon" width="50px" />
         </Link>
+          <p style={{width: "100px", textAlign: "center"}} data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
       </div>
     );
   }
