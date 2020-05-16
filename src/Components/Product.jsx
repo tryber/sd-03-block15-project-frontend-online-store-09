294c18adb60241e30203588457bdb70d64c5b7ab
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-05-16 17:55:50 -0300

    minor changes

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,6 +1,5 @@
-import React, { Component } from "react";
-import ListDetails from "./LIstDetails";
-// import {Link} from 'react-router-dom'
+import React, { Component } from 'react';
+import ListDetails from './LIstDetails';
 
 export class Product extends Component {
   constructor(props) {
@@ -18,8 +17,8 @@ export class Product extends Component {
       product: { title, price, thumbnail, id },
     } = this.props;
     return (
-      <div className="product">
-        <div style={{ width: "18rem" }} data-testid="product">
+      <div className='product'>
+        <div style={{ width: '18rem' }} data-testid='product'>
           <img src={thumbnail} alt={`${title} thumbnail`} />
           <h5>{title}</h5>
           <h3>
@@ -35,7 +34,7 @@ export class Product extends Component {
               }}
             />
             <button
-              data-testid="product-add-to-cart"
+              data-testid='product-add-to-cart'
               onClick={() =>
                 this.addToLocalStorage(title, price, thumbnail, id)
               }
