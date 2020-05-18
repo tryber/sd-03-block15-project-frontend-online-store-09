2744d55aed8205eee3279314e4a8c0c34a48c13e
Cardoso <lsc.cardosoo@gmail.com>
2020-05-18 10:51:36 -0300

    Code Climate

--- src/Components/Product_Components/Product.jsx
+++ src/Components/Product_Components/Product.jsx
@@ -7,9 +7,7 @@ import veiculo from '../../Components/veiculo.svg';
 export class Product extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      freeShipping: this.props.product.shipping.free_shipping,
-    }
+    this.state = { freeShipping: this.props.product.shipping.free_shipping };
     this.addToLocalStorage = this.addToLocalStorage.bind(this);
   }
 
@@ -18,16 +16,21 @@ export class Product extends Component {
     addToCart(product);
   }
 
+  renderFreeShippingIcon() {
+    return (
+      this.state.freeShipping &&
+      <img
+        className="freeShipping"
+        data-testid="free-shipping"
+        src={veiculo} alt="Ícone frete grátis" width="15%"
+      />
+    );
+  }
   render() {
     const { product: { title, price, thumbnail } } = this.props;
     return (
       <div className="product">
-          {this.state.freeShipping &&
-            <img
-              className="freeShipping"
-              data-testid="free-shipping"
-              src={veiculo} alt="Ícone frete grátis" width="15%" />
-          }
+        {this.renderFreeShippingIcon()}
         <div data-testid="product">
           <img
             className="product-image"
