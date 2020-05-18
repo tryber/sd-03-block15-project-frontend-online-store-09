511ba446658930972f2e5e9581802dbc863e9dc5
Cardoso <lsc.cardosoo@gmail.com>
2020-05-18 11:50:32 -0300

    CartProduct's css added without display flex

--- src/Components/Cart_Components/CartCard.jsx
+++ src/Components/Cart_Components/CartCard.jsx
@@ -25,7 +25,7 @@ class CartCard extends Component {
     const { quantity } = this.state;
 
     return (
-      <div className="product">
+      <div className="cartProduct">
         <h3 data-testid="shopping-cart-product-name">{title}</h3>
         <img src={thumbnail} alt={title} />
         <p data-testid="shopping-cart-product-quantity">{`Quantidade: ${quantity}`}</p>
