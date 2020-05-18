038a7842fa62cf6eff89b1b4fb88bc388fa5e2ec
Cardoso <lsc.cardosoo@gmail.com>
2020-05-18 10:45:06 -0300

    Free shipping added in product component

--- src/Components/Product_Components/Product.jsx
+++ src/Components/Product_Components/Product.jsx
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import ListDetails from '../List_Details/LIstDetails';
 import addToCart from '../../services/addToCard';
 import './ProductList.css';
+import veiculo from '../../Components/veiculo.svg';
 
 export class Product extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      freeShipping: this.props.product.shipping.free_shipping,
+    }
     this.addToLocalStorage = this.addToLocalStorage.bind(this);
   }
 
@@ -18,6 +22,12 @@ export class Product extends Component {
     const { product: { title, price, thumbnail } } = this.props;
     return (
       <div className="product">
+          {this.state.freeShipping &&
+            <img
+              className="freeShipping"
+              data-testid="free-shipping"
+              src={veiculo} alt="Ícone frete grátis" width="15%" />
+          }
         <div data-testid="product">
           <img
             className="product-image"
